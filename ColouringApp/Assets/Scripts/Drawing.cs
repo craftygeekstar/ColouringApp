c75732f9c7771ff279a8a42b514d84c3b71bc92a
Orla <107683831+craftygeekstar@users.noreply.github.com>
2022-09-27 13:01:02 +0100

    Working App!
    Marker moves with the controller.
    Trigger button starts the drawing.
    But frame rate is so slow it is unplayable.

--- ColouringApp/Assets/Scripts/Drawing.cs
+++ ColouringApp/Assets/Scripts/Drawing.cs
@@ -1,16 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR;
 using UnityEngine.InputSystem;
 
 
 public class Drawing : MonoBehaviour
 {
     public InputActionReference drawReference = null;
-    public GameObject marker;
-    public GameObject cube;
-    public GameObject tip;
+    // public GameObject marker;
+    // public GameObject cube;
+    // public GameObject tip;
     
     private void Awake()
     {
@@ -25,9 +24,11 @@ public class Drawing : MonoBehaviour
     private void Draw(InputAction.CallbackContext context)
     {
         //marker.GetComponent<WBMarker>().enabled = !marker.GetComponent<WBMarker>().enabled;
-        bool isActive = !cube.activeSelf;
-        cube.SetActive(isActive);
-        bool isActiveTip = !tip.activeSelf;
-        tip.SetActive(isActiveTip);
+        // bool isActive = !gameObject.activeSelf;
+        // cube.SetActive(isActive);
+        // bool isActiveTip = !tip.activeSelf;
+        // tip.SetActive(isActiveTip);
+        bool isActive = !gameObject.activeSelf;
+        gameObject.SetActive(isActive);
     }
 }
